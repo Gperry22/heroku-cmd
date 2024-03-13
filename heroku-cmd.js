259b63c9f5c0dfc6679b5dd884d774429095bcaf
CodingINS <coding@Gabes-Mac-Studio-4.local>
2024-03-13 18:04:06 -0400

    first commit

---
+++ heroku-cmd.js
@@ -0,0 +1,11 @@
+// 1. cd to root of project
+// 2. git init
+// 3. git add.
+// 4. git commit - m "Your commit message"
+// 5. heroku create
+// 6. git remote - v
+// 7. git push heroku master
+
+// For additional resources:
+// https://devcenter.heroku.com/articles/git#creating-a-heroku-remote
+
